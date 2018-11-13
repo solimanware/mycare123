50926e14d1834e5b7cd1466ff035094bf5c22f27
Osama Mohamed <dev.osama.mohamed@gmail.com>
2018-11-13 11:52:28 +0200

    debug db config

--- models/index.js
+++ models/index.js
@@ -6,6 +6,8 @@ const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.json')[env];
+
+console.log('config: ' + JSON.stringify(config))
 const db = {};
 
 let sequelize;
